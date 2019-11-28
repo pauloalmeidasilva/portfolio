f6d44a25da6957e7fef459b8e2db4340e205ea81
Paulo Ricardo Almeida da Silva <paulocdjsilva@gmail.com>
2019-11-28 19:52:48 -0300

    Documentação do controller através de comentários

--- application/controllers/Painel.php
+++ application/controllers/Painel.php
@@ -2,12 +2,32 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Painel extends CI_Controller {
+	/**
+	 * Controller Painel
+	 * 
+	 * Descrição:
+	 * 		Controller responsável pelo obtenção e manipulação de dados solicitados na tela do painel principal do sistema.
+	 * 
+	 * Métodos Implementados:
+	 * 		__construtor;
+	 * 		atualizaBanco;
+	 * 		index;
+	 * 
+	 * Orientação para implementação do controller:
+	 * 		=> Informar os atributos utilizados no controller;
+	 * 		=> Método Construtor;
+	 * 		=> Métodos privados;
+	 * 		=> Métodos públicos referente à página;
+	 */
 
 	public function __construct(){
 		parent::__construct();
 	}
 
 	private function atualizaBanco(){
+		/**
+		 * Método responsável pala atualização da versão do banco de dados pelo migrations.
+		 */
 		$this->load->library('migration');
 
 		if ($this->migration->current() === FALSE){
@@ -15,7 +35,8 @@ class Painel extends CI_Controller {
 		}
 	}
 
-	public function index() {
+	public function index(){
+		$this->atualizaBanco();
 		$dados = array(
 			'title' => 'Painel',
 			'stylesheets' => array(
